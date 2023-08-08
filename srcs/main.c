90ca2d36c1d0adfed36231ca41aad3fe4b9241e3
dreywesson <dreywesson@gmail.com>
2023-08-08 12:59:25 +0100

    testing

--- srcs/main.c
+++ srcs/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: conyejib <conyejib@student.42wolfsburg.    +#+  +:+       +#+        */
+/*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 11:47:59 by conyejib          #+#    #+#             */
-/*   Updated: 2023/08/07 15:35:13 by conyejib         ###   ########.fr       */
+/*   Updated: 2023/08/08 12:59:15 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ static int	expose_handle(t_fdf *fdf);
 int	main(int argc, char **argv)
 {
 	char	*file_name;
-	t_fdf	*fdf;
+	t_fdf	**fdf;
 
 	if (argc != 2)
 		error(1); ///all program error handled error(1) flags an input error
