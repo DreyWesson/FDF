ff5ae4639632e88c2ebb0dd5f89eca6efdb6af19
dreywesson <dreywesson@gmail.com>
2023-08-08 13:08:20 +0100

    implement pass by ref

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: doduwole <doduwole@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/21 11:47:59 by conyejib          #+#    #+#             */
-/*   Updated: 2023/08/08 12:59:59 by doduwole         ###   ########.fr       */
+/*   Updated: 2023/08/08 13:05:20 by doduwole         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ static int	expose_handle(t_fdf *fdf);
 int	main(int argc, char **argv)
 {
 	char	*file_name;
-	t_fdf	*****fdf;
+	t_fdf	**fdf;
 
 	if (argc != 2)
 		error(1); ///all program error handled error(1) flags an input error
 	file_name = argv[1]; // saving arguement to filename
-	fdf = init_fdf(file_name); //
-	render(fdf);
-	mlx_key_hook(fdf->win, &key_handle, fdf);
-	mlx_expose_hook(fdf->win, &expose_handle, fdf);
-	mlx_loop(fdf->mlx);
+	init_fdf(file_name, fdf); //
+	render((*fdf));
+	mlx_key_hook((*fdf)->win, &key_handle, *fdf);
+	mlx_expose_hook((*fdf)->win, &expose_handle, *fdf);
+	mlx_loop((*fdf)->mlx);
 }
 
 static int	expose_handle(t_fdf *fdf)
